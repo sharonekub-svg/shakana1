c0ef04a4bf24f86d43112dfff5dea379af8f3828
sharonekub-svg <sharonekub@gmail.com>
2026-04-29 23:17:28 +0300

    Fix Expo web build resolution

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,6 +1,18 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname, { isCSSEnabled: true });
+const workspaceRoot = path.resolve(__dirname, '../..');
+
+config.watchFolders = Array.from(new Set([...(config.watchFolders ?? []), workspaceRoot]));
+config.resolver = {
+  ...config.resolver,
+  unstable_enableSymlinks: true,
+  nodeModulesPaths: [
+    path.resolve(__dirname, 'node_modules'),
+    path.resolve(workspaceRoot, 'node_modules'),
+  ],
+};
 
 module.exports = withNativeWind(config, { input: './global.css' });
